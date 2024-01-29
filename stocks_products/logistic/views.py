c6cddadb25908a570ef4871b27550540ab381550
menshikov <matveydrobot15@gmail.com>
2024-01-29 20:56:36 +0300

    Initial commit

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,15 @@
+from rest_framework.viewsets import ModelViewSet
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset
